7c837425b756f10db63300bf6865d07ba78af8b0
decpare <decpare@yandex.ru>
2015-05-17 21:39:55 +0300

    Update. Meets the requirements PEP8

--- src/alphabet.py
+++ src/alphabet.py
@@ -6,22 +6,25 @@ __version__ = "1.0"
 
 # ----------------------------- #
 
+
 def create_user_alphabet(key, input_alphabet):
-    if key == "": return input_alphabet
+    if key == "":
+        return input_alphabet
     # thanks python.su - paranax
     # if use 'set' the list is sorted
-    key = [e for i,e in enumerate(key) if e not in key[:i]] 
+    key = [e for i, e in enumerate(key) if e not in key[:i]]
 
     for i in range(len(key)):
         key[i] = key[i].lower()
-        if key[i] in input_alphabet: del input_alphabet[input_alphabet.index(key[i])] 
+        if key[i] in input_alphabet:
+            del input_alphabet[input_alphabet.index(key[i])]
 
     return key + input_alphabet
 
 eng_alphabet = list("abcdefghijklmnopqrstuvwxyz")
 rus_alphabet = list("абвгдеёжзийклмнопрстуфхцчшщъыьэюя")
-user_alphabet = "" 
+user_alphabet = ""
 
 if __name__ == "__main__":
-	user_alphabet = create_user_alphabet("danil", eng_alphabet)
-	print(user_alphabet)
+    user_alphabet = create_user_alphabet("danil", eng_alphabet)
+    print(user_alphabet)
